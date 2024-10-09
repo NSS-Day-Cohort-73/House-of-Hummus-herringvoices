5e924392e6c46e59a3cb9277dd0b8bedef9120f5
herringvoices <nalherring@gmail.com>
2024-10-09 13:09:29 -0500

    added id attribute to input elements in renderOptions()

--- scripts/renderOptions.js
+++ scripts/renderOptions.js
@@ -4,19 +4,21 @@ export const renderOptions = async (type) => {
 
     let html = responseArray.map(item => {
         if (item === responseArray[0]) {
+            //creates a checked input for the first item in the map
             return `
             <div>
                 <label for='${type}${item.id}'>
-                    <input checked type='radio' name='${type}' value='${item.id}'>
-                    ${item.name} <!-- Optionally include item name if available -->
+                    <input checked type='radio' name='${type}' id='${type}${item.id}' value='${item.id}'>
+                    ${item.name}
                 </label>
             </div>
             `
         } else {
+            //creates the rest of the inputs without 'checked'
             return `
             <div>
                 <label for='${type}${item.id}'>
-                    <input type='radio' name='${type}' value='${item.id}'>
+                    <input type='radio' id='${type}${item.id}' name='${type}' value='${item.id}'>
                     ${item.name}
                 </label>
             </div>
